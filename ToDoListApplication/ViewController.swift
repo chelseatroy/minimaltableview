14c55ad6c7ae93637a6c2c0fa83e5b812cc00385
chelseatroy <chelsea.dommert@gmail.com>
2018-12-06 16:16:22 -0600

    Initial Commit

---
+++ ToDoListApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoListApplication
+//
+//  Created by Chelsea Dommert on 12/6/18.
+//  Copyright © 2018 Chelsea Troy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
